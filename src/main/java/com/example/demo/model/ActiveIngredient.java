ad5048c00e85b8aa0739371327a8b346542b6a9b
bala-amypo <bala@amypo.in>
2025-12-27 08:31:12 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -3,20 +3,19 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "active_ingredient")
 public class ActiveIngredient {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    public Long getId() {
-        return id;
-    }
+    public ActiveIngredient() {}
+    public ActiveIngredient(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
