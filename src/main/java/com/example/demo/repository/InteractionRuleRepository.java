4bd90fe4a6f4d4f9a42c4e5a83f70638c8ad1437
bala-amypo <bala@amypo.in>
2025-12-27 08:34:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/InteractionRuleRepository.java
+++ src/main/java/com/example/demo/repository/InteractionRuleRepository.java
@@ -1,20 +1,29 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.InteractionRule;
+import com.example.demo.entity.InteractionRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InteractionRuleRepository
         extends JpaRepository<InteractionRule, Long> {
 
     @Query("""
-SELECT r FROM InteractionRule r
-WHERE r.ingredientA.id = :id OR r.ingredientB.id = :id
-""")
-List<InteractionRule> findByIngredientId(@Param("id") Long id);
+    SELECT r FROM InteractionRule r
+    WHERE (r.ingredientA.id = :id1 AND r.ingredientB.id = :id2)
+       OR (r.ingredientA.id = :id2 AND r.ingredientB.id = :id1)
+    """)
+    Optional<InteractionRule> findRuleBetweenIngredients(
+            @Param("id1") Long id1,
+            @Param("id2") Long id2
+    );
 
-   
+    @Query("""
+    SELECT r FROM InteractionRule r
+    WHERE r.ingredientA.id = :id OR r.ingredientB.id = :id
+    """)
+    List<InteractionRule> findByIngredientId(@Param("id") Long id);
 }
